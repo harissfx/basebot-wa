d94da77606c7ef1fa5d669660e38881324316015
harissfx <hs6115308@gmail.com>
2026-06-04 17:55:38 +0700

    fix pesan menu

--- src/utils/fquoted.js
+++ src/utils/fquoted.js
@@ -1,23 +1,23 @@
 const config = require('../config');
-const fakeOrder = {
-key: {
-participant: '0@s.whatsapp.net',
-},
-message: {
-requestPaymentMessage: 
-{
-currencyCodeIso4217: 'USD',
-amount1000: '1000000000',
-requestFrom: '6285731811079@s.whatsapp.net',
-noteMessage: {
-extendedTextMessage: {
-text: `*${config.botName}*`,
-}
-},
-expiryTimestamp: '0',
-amount: { value: '1000000000', offset: 1000, currencyCode: 'USD' }
-}
-}
-};
+
+const fakeOrder = ({ pushname = '', salam = '' } = {}) => ({
+    key: {
+        participant: '0@s.whatsapp.net',
+    },
+    message: {
+        requestPaymentMessage: {
+            currencyCodeIso4217: 'USD',
+            amount1000: '1000000000',
+            requestFrom: '6285731811079@s.whatsapp.net',
+            noteMessage: {
+                extendedTextMessage: {
+                    text: `Hay Kak ${pushname} 👋 Selamat ${salam}`,
+                }
+            },
+            expiryTimestamp: '0',
+            amount: { value: '1000000000', offset: 1000, currencyCode: 'USD' }
+        }
+    }
+});
 
 module.exports = { fakeOrder };
