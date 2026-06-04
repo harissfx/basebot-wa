3f9fe929be42e663a235c541bee5660a897d2efb
harissfx <hs6115308@gmail.com>
2026-06-04 13:40:45 +0700

    fix fitur downloader

--- src/utils/fquoted.js
+++ src/utils/fquoted.js
@@ -5,25 +5,24 @@ const config = require('../config');
 const thumbnail = fs.readFileSync(path.join(__dirname, '../media/logo.png'));
 
 const fakeOrder = {
-    key: {
-        participant: '0@s.whatsapp.net',
-    },
-    'message': {
-        "interactiveMessage": {
-            "header": {
-                "hasMediaAttachment": [],
-                "jpegThumbnail": thumbnail,
-            },
-            "nativeFlowMessage": {
-                "buttons": [
-                    {
-                        "name": "review_and_pay",
-                        "buttonParamsJson": "{\"currency\":\"IDR\",\"external_payment_configurations\":[{\"uri\":\"\",\"type\":\"payment_instruction\",\"payment_instruction\":\"hey ini test\"}],\"payment_configuration\":\"\",\"payment_type\":\"\",\"total_amount\":{\"value\":100000,\"offset\":100},\"reference_id\":\"4MX98934S0D\",\"type\":\"physical-goods\",\"order\":{\"status\":\"pending\",\"description\":\"\",\"subtotal\":{\"value\":100000,\"offset\":100},\"items\":[{\"retailer_id\":\"60135576635\",\"product_id\":\"60135576635\",\"name\":\"𝑃𝑜𝑤𝑒𝑟𝑒𝑑 𝐵𝑦 𝐻𝑎𝑛𝑧 𝑂𝑓𝑐\",\"amount\":{\"value\":100000,\"offset\":100},\"quantity\":1111111}]}}"
-                    }
-                ]
-            }
-        }
-    }
-};
+key: {
+participant: '0@s.whatsapp.net',
+},
+message: {
+requestPaymentMessage: 
+{
+currencyCodeIso4217: 'USD',
+amount1000: '1000000000',
+requestFrom: '6285731811079@s.whatsapp.net',
+noteMessage: {
+extendedTextMessage: {
+text: `bot`,
+}
+},
+expiryTimestamp: '0',
+amount: { value: '1000000000', offset: 1000, currencyCode: 'USD' }
+}
+}
+}
 
 module.exports = { fakeOrder };
