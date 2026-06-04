c9a27cee96e877fe03d8072fd661a8cb0931ffce
harissfx <hs6115308@gmail.com>
2026-06-04 13:19:49 +0700

    fix fitur downloader

--- src/commands/interactive.js
+++ src/commands/interactive.js
@@ -129,24 +129,63 @@ const handler = async (ctx) => {
             break;
     case 'pay': 
             const imagePathh = path.join(__dirname, '../media/logo.png');
+                    const orderData = {
+            currency: "USD",
+            external_payment_configurations: [
+                {
+                    url: "https://jancok.com",
+                    type: "payment_instruction",
+                    payment_instruction: "Dummy payment by Haris Sfx"
+                }
+            ],
+            payment_configuration: "config",
+            payment_type: "true",
+            total_amount: {
+                value: 100000000,
+                offset: 2
+            },
+            reference_id: `referensi id`,
+            type: "physical-goods",
+            order: {
+                status: "delivered",
+                description: `deskripsi`,
+                subtotal: {
+                    value: 100000000,
+                    offset: 100
+                },
+                items: [
+                    {
+                        retailer_id: "60135576635",
+                        product_id: "60135576635",
+                        
+                        name: `name`,
+                        amount: {
+                            value: 100000000,
+                            offset: 2
+                        },
+                        quantity: `999`,
+                    }
+                ]
+            }
+        };
             await ctx.sendInteractive({
-    'message': {
-	"interactiveMessage": {
-						"header": {
-						
-							"hasMediaAttachment": [],
-							"jpegThumbnail": fs.readFileSync(imagePathh),
-													},
-						"nativeFlowMessage": {
-							"buttons": [
-								{
-									"name": "review_and_pay",
-									"buttonParamsJson": "{\"currency\":\"IDR\",\"external_payment_configurations\":[{\"uri\":\"\",\"type\":\"payment_instruction\",\"payment_instruction\":\"hey ini test\"}],\"payment_configuration\":\"\",\"payment_type\":\"\",\"total_amount\":{\"value\":100000,\"offset\":100},\"reference_id\":\"4MX98934S0D\",\"type\":\"physical-goods\",\"order\":{\"status\":\"pending\",\"description\":\"\",\"subtotal\":{\"value\":100000,\"offset\":100},\"items\":[{\"retailer_id\":\"60135576635\",\"product_id\":\"60135576635\",\"name\":\"𝑃𝑜𝑤𝑒𝑟𝑒𝑑 𝐵𝑦 𝐻𝑎𝑛𝑧 𝑂𝑓𝑐\",\"amount\":{\"value\":100000,\"offset\":100},\"quantity\":1111111}]}}"
-								}
-							]
-			}
-            
-}}
+                        // Ini payload mentah untuk relayMessage
+            orderMessage:  {
+            interactiveMessage: {
+                header: {
+                    hasMediaAttachment: [],
+                    jpegThumbnail: thumbnail,
+                },
+                nativeFlowMessage: {
+                    buttons: [
+                        {
+                            name: "review_and_pay",
+                            buttonParamsJson: JSON.stringify(orderData)
+                        }
+                    ]
+                }
+            }
+        },
 });
             break;
         
