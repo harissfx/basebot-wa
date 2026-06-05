2078e78d905c2343aede0a7ddabaa0fb759b94bc
harissfx <hs6115308@gmail.com>
2026-06-05 17:55:13 +0700

    fix parameter

--- src/commands/interactive.js
+++ src/commands/interactive.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const config = require('../config');
-const { getImage } = require('../utils/helper');s
-
+const { getImage } = require('../utils/helper');
 const handler = async (m) => {
     const { command } = m
 
